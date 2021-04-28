98468d82801c1b90769af7f174f51252975a21c0
iftekhartahir <66146458+iftekhartahir@users.noreply.github.com>
2021-04-28 22:26:46 +0600

    Initial commit

---
+++ learn-git/ascii-portfolio.sh
@@ -0,0 +1,16 @@
+git add portrait.txt
+			
+git status
+
+git commit -m "change in beard"
+
+git add .
+
+git commit -m "changes in date"
+
+git add .
+
+git reset HEAD house.txt
+
+git commit -m "house removed"
+
