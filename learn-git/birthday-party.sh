2ee9a590f65a72cb0e9467f672d7c8fa171ee865
iftekhartahir <66146458+iftekhartahir@users.noreply.github.com>
2021-04-28 22:30:13 +0600

    Initial commit

---
+++ learn-git/birthday-party.sh
@@ -0,0 +1,25 @@
+git branch
+
+git branch -D moma
+			
+git branch -D whitney
+
+git branch unoredred-list
+
+git add index.html
+
+git commit -m "unordered-list"
+
+git checkout master
+
+git merge unordered-list
+
+git branch big-heading
+
+git add index.html
+
+git commit -m "Big heading"
+
+git checkout master
+
+git merge big-heading
