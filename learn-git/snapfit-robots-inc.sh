657835c2cd487fdd8ed4925edb0932cbf68e4dbf
iftekhartahir <66146458+iftekhartahir@users.noreply.github.com>
2021-04-28 22:17:42 +0600

    Initial commit

---
+++ learn-git/snapfit-robots-inc.sh
@@ -0,0 +1,20 @@
+git init
+
+git status
+
+git add disclaimer.txt	
+git add instructions.txt
+git add warranty.txt
+
+git status
+
+git commit -m "SnapFit Robots Inc"
+
+git log
+
+git add disclaimer.txt
+
+git commit -m "SnapFit Robots Inc (Updated)"
+
+git log
+
