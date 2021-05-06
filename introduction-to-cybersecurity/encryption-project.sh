84acc78adebf7c503e6a1628c755739390658c3a
iftekharmickey <66146458+iftekharmickey@users.noreply.github.com>
2021-05-06 16:08:40 +0600

    Initial commit

---
+++ introduction-to-cybersecurity/encryption-project.sh
@@ -0,0 +1,17 @@
+ls
+
+cat letter_1.txt
+
+base64 -d letter_2.txt
+
+cat letter_3.txt | tr "P-ZA-Op-za-o" "A-Za-z"
+
+unzip letter_4.zip
+
+cat letter_4.txt
+
+ls letter_5
+
+cat letter_5/letter_5.txt
+
+./letter_5/disable_the_lasers.sh
