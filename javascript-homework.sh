eee546acbadcf0944a2dcd7a188f333a4530fe89
iftekharmickey <66146458+iftekharmickey@users.noreply.github.com>
2021-05-18 02:45:44 +0600

    Create javascript-homework.sh

---
+++ javascript-homework.sh
@@ -0,0 +1,14 @@
+git clone maryrose-hw student-hw
+
+cd student-hw
+
+git branch comment-hw
+			
+git checkout comment-hw
+
+git add homework.js
+
+git commit -m "Added a comment"
+
+git push origin comment-hw
+
