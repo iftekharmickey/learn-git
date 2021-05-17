3405c6abedf75853db35502c22f9dba49c575f87
iftekharmickey <66146458+iftekharmickey@users.noreply.github.com>
2021-05-18 02:14:39 +0600

    Update sals-shopping.py

--- learn-python-3/sals-shopping.py
+++ learn-python-3/sals-shopping.py
@@ -10,11 +10,13 @@ elif weight <= 10:
   cost_ground = (weight*4.00 + 20.00)
 else:
   cost_ground = (weight*4.75 + 20.00)
+
 print("Ground Shipping: $", cost_ground)
 
 # groundshippingpremium
 
 cost_ground_premium = 125.00
+
 print("Ground Shipping Premium: $", cost_ground_premium)
 
 # droneshipping
@@ -27,4 +29,5 @@ elif weight <= 10:
   cost_drone = (weight*12.00)
 else:
   cost_drone = (weight*14.25)
+
 print("Drone Shipping: $", cost_drone)
